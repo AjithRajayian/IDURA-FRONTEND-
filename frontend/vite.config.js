a10410ccaffbde1221ca50d4259a4d11a8a3d174
AjithRajayian <ajithrajayyan99@gmail.com>
2025-03-07 12:56:44 +0530

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,4 @@
 import path from 'path';
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -11,10 +10,9 @@ export default ({ mode }) => {
       ? process.env.VITE_BACKEND_SERVER
       : 'https://idura-backend.onrender.com/';
 
-  const config = {
+  return defineConfig({
     plugins: [react()],
     resolve: {
-      base: '/',
       alias: {
         '@': path.resolve(__dirname, 'src'),
       },
@@ -29,6 +27,5 @@ export default ({ mode }) => {
         },
       },
     },
-  };
-  return defineConfig(config);
+  });
 };
